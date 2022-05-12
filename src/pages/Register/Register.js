bb0d91ebe90335ee5f79170d266e32c8dbde9f4f
Augusto Souza <augusto_souza@edu.unifil.br>
2022-05-12 10:42:21 -0300

    useState form register

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,6 +1,68 @@
 import styles from "./Register.modules.css";
+import { useState, useEffect } from "react";
 const Register = () => {
-  return <div>Register</div>;
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmSenha, setConfirmSenha] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <h1>Cadastre-se</h1>
+      <p>Crie seu usuário e compartilhe suas histórias!</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Nome:</span>
+          <input
+            type="text"
+            name="nome"
+            placeholder="Nome de usuário"
+            required
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            placeholder="E-mail"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            placeholder="Insira uma senha"
+            required
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Confirmação de senha:</span>
+          <input
+            type="password"
+            name="password"
+            placeholder="Confirme sua senha"
+            required
+            value={confirmSenha}
+            onChange={(e) => setConfirmSenha(e.target.value)}
+          />
+        </label>
+        <button className="btn">Cadastrar</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
